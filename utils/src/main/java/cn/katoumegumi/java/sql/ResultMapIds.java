8112dbbe6b4c9c465e7be85d4e8ba58aa41b2284
ws <1048058229@qq.com>
2020-02-28 16:52:44 +0800

    修复一些功能

--- utils/src/main/java/cn/katoumegumi/java/sql/ResultMapIds.java
+++ utils/src/main/java/cn/katoumegumi/java/sql/ResultMapIds.java
@@ -1,8 +1,11 @@
 package cn.katoumegumi.java.sql;
 
+import cn.katoumegumi.java.common.Encryption;
 import cn.katoumegumi.java.common.WsBeanUtis;
+import com.alibaba.fastjson.JSON;
 import lombok.Data;
 
+import javax.swing.text.html.parser.Entity;
 import java.util.*;
 
 /**
@@ -11,25 +14,44 @@ import java.util.*;
 @Data
 public class ResultMapIds {
 
-    private Set<Object> set = new HashSet<>();
+    private List<Object> set;
+
+    private String md5;
 
 
     public ResultMapIds(Map map){
-        Collection collection = map.values();
+        set = new ArrayList<>(map.size() << 1);
+        Set<Map.Entry> entities = map.entrySet();
+        Iterator<Map.Entry> iterable = entities.iterator();
+        while (iterable.hasNext()){
+            Map.Entry entry = iterable.next();
+            Object o = entry.getValue();
+            if(o != null){
+                if(WsBeanUtis.isBaseType(o.getClass())){
+                    set.add(entry.getKey());
+                    set.add(entry.getValue());
+                }
+            }
+        }
+        //md5 = Encryption.md5Encoder(JSON.toJSONString(set));
+
+        /*Collection collection = map.values();
         Iterator iterator = collection.iterator();
         while (iterator.hasNext()){
             Object o = iterator.next();
-            if(WsBeanUtis.isBaseType(o.getClass())){
-                set.add(o);
+            if(o != null){
+                if(WsBeanUtis.isBaseType(o.getClass())){
+                    set.add(o);
+                }
             }
-        }
+        }*/
     }
 
 
     @Override
     public boolean equals(Object o) {
         if(o instanceof ResultMapIds){
-            Set set2 = ((ResultMapIds) o).getSet();
+            List set2 = ((ResultMapIds) o).getSet();
             if(set2.size() != set.size()){
                 return false;
             }
@@ -44,6 +66,7 @@ public class ResultMapIds {
                 }
             }
             return true;
+            //return md5.equals(((ResultMapIds) o).md5);
 
         }else {
             return false;
@@ -52,7 +75,8 @@ public class ResultMapIds {
 
     @Override
     public int hashCode() {
-        return Arrays.hashCode(new ArrayList(set).toArray());
+        //return md5.hashCode();
+        return Arrays.hashCode(set.toArray());
 
     }
 }
