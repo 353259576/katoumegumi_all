6773cca440316c7a92ac58b7222f603fafb6fa1f
ws <1048058229@qq.com>
2023-08-09 18:20:54 +0800

    去掉无用的代码

--- sql_utils/src/main/java/cn/katoumegumi/java/sql/handler/model/UpdateSqlEntity.java
+++ sql_utils/src/main/java/cn/katoumegumi/java/sql/handler/model/UpdateSqlEntity.java
@@ -11,10 +11,6 @@ public class UpdateSqlEntity {
 
     private String updateSql;
 
-    private List<FieldColumnRelation> usedField;
-
-    private List<FieldColumnRelation> idList;
-
     private List<SqlParameter> valueList;
 
 
@@ -24,22 +20,6 @@ public class UpdateSqlEntity {
 
     public void setUpdateSql(String updateSql) {
         this.updateSql = updateSql;
-    }
-
-    public List<FieldColumnRelation> getUsedField() {
-        return usedField;
-    }
-
-    public void setUsedField(List<FieldColumnRelation> usedField) {
-        this.usedField = usedField;
-    }
-
-    public List<FieldColumnRelation> getIdList() {
-        return idList;
-    }
-
-    public void setIdList(List<FieldColumnRelation> idList) {
-        this.idList = idList;
     }
 
     public List<SqlParameter> getValueList() {
